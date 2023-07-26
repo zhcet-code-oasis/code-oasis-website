377fd3e4bb4ac64e329f1c25f528a227553a0049
Tayyab Ilyas <110786036+tayyab-ilyas@users.noreply.github.com>
2023-07-27 01:54:49 +0530

    ci/cd process

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [react(), tailwind()]
+  site: 'https://zhcet-code-oasis.github.io',
+  base: '/code-oasis-website',
 });
