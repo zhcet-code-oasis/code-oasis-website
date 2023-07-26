5373d6ff3b768da45654f6c12b4d399c4aa1ea7f
Tayyab <tayyabilyas963@gmail.com>
2023-07-27 05:19:45 +0530

    swiching to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify/functions";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()] //site: 'https://zhcet-code-oasis.github.io',
   //base: '/code-oasis-website',
   ,
+
   output: "server",
-  adapter: netlify()
+  adapter: vercel()
 });
