a374b15e5e8e3f8e7ade7f646f4ceb0802ecc48e
Tayyab <tayyabilyas963@gmail.com>
2023-07-27 04:54:56 +0530

    netlify.toml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/functions";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
-  //site: 'https://zhcet-code-oasis.github.io',
+  integrations: [react(), tailwind()] //site: 'https://zhcet-code-oasis.github.io',
   //base: '/code-oasis-website',
+  ,
+  output: "server",
+  adapter: netlify()
 });
