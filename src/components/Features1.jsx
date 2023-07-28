729de3578b3f45803ded3e3554529369b09904f9
Rajul Jha <rajuljha49@gmail.com>
2023-07-28 18:58:51 +0530

    Changed feature 1 pics

--- src/components/Features1.jsx
+++ src/components/Features1.jsx
@@ -1,9 +1,9 @@
 import { motion } from "framer-motion";
 
-import feature1 from "../assets/images/feature1.jpg";
-import feature2 from "../assets/images/feature2.jpg";
-import feature3 from "../assets/images/feature3.jpg";
-import feature4 from "../assets/images/feature4.jpg";
+import feature1 from "../assets/images/feature-new1.jpg";
+import feature2 from "../assets/images/feature-new2.jpg";
+import feature3 from "../assets/images/feature-new3.jpg";
+import feature4 from "../assets/images/feature-new4.jpg";
 import { CheckArrowIcon } from "../assets/icons/CheckArrowIcon";
 
 export const Features1 = () => {
