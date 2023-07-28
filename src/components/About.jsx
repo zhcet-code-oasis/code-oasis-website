9a91fac4208906c1d3485d2a8c839dbb9f7b6449
Rajul Jha <rajuljha49@gmail.com>
2023-07-28 18:06:59 +0530

    Add FAQ's

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,24 +3,24 @@ import { motion } from "framer-motion";
 
 const AboutData = [
   {
-    question: "Can I upgrade or downgrade my plan at any time?",
+    question: "Do I need to have tech knowledge to get involved?",
     answer:
-      "Yes, you can easily upgrade or downgrade your plan at any time. Simply navigate to the account settings in your dashboard and choose the desired plan. The changes will be reflected immediately, and any adjustments in pricing will be applied on your next billing cycle. Our support team is more than happy to provide guidance and recommendations.",
+      "No. We welcome anyone who is interested in contributing to the project. We have a diverse community of contributors with varying levels of experience and expertise. We encourage you to join us and learn as you go!",
   },
   {
-    question: "How to claim your 25% discount offer?",
+    question: "I am an absolute beginner at OSS. Can I still contribute?",
     answer:
-      "To claim your 25% discount, simply sign up for an account and enter the promotional code at checkout. The discount will be applied automatically to your purchase.",
+      "Yes! We have a dedicated community of mentors who will help you get started and guide you through the process. We also have a number of resources available to help you learn the basics of open source software development.",
   },
   {
-    question: "What's your refund policy?",
+    question: "I am not a student of ZHCET. Can I be a part of the community?",
     answer:
-      "We offer a 30-day money-back guarantee on all our plans. If you're not satisfied with our product, simply contact our support team within 30 days of purchase for a full refund.",
+      "Yes! We welcome anyone who is interested in contributing to Open source software. Whether you are a student or a working professional, join us! If you have the knowledge, then radiate it to others. It's about the community, not the college.",
   },
   {
-    question: "How to get support for the product?",
+    question: "What can I do for the community?",
     answer:
-      "Our dedicated support team is here to help. You can reach out to us through the contact form on our website, send an email, or engage with us via live chat. We'll be happy to assist you with any questions or concerns you may have",
+      "You can contribute to the community in many ways. You can contribute to the codebase, documentation, or help us in organizing events. You can also help us in promoting the community and spreading awareness about open source software. Whether you wanna stick to your strengths or try something new, we have a place for you in our community.",
   },
 ];
 
